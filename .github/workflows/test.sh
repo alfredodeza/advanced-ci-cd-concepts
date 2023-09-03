10fc9dd7151e72d29bd0bfc8a2895a38369bba3d
Alfredo Deza <alfredo@deza.pe>
2023-09-03 15:39:49 -0400

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ -z "${CI_SECRET_TOKEN}" ]]; then
-  echo "We are good to go with the secret token!"
-else
   echo "We don't have the secret token, can't continue"
   exit 1
+else
+  echo "We are good to go with the secret token!"
 fi 
